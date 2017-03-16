cff1b2f6e23314e59ce5012cb4fb6a7e1d64f299
v-jzingh <yzzj1126@qq.com>
2017-03-16 17:29:48 +0800

    获取系统用户

--- Praise2017/Praise2017/Controllers/HomeController.cs
+++ Praise2017/Praise2017/Controllers/HomeController.cs
@@ -1,24 +1,49 @@
 ﻿using System.Linq;
 using System.Web.Mvc;
+using System.Web.Security;
+using System.Web;
 
 namespace Praise2017.Controllers
 {
     public class HomeController : Controller
     {
+        //获取当前登录用户
+        string user = System.Environment.UserName;
+
+
 
         private EFDbContext db = new EFDbContext();
-       // var db = new EFDbContext;
+
 
         public PartialViewResult DetailList()
         {
             var query = from b in db.Details
-                        //orderby b.UpdateDate
+                            //orderby b.UpdateDate
                         select b;
-            
+
             return PartialView(query);
         }
 
 
+        //显示本月排行
+        //public PartialViewResult TopPraise()
+        //{
+
+        //}
+
+        ////显示当前用户信息
+        public PartialViewResult PerInfo()
+        {
+            //得到赞总数
+            int Got=from b in db.Details
+                       where b.ToWho==user
+
+
+            //发出赞总数
+
+            //当前用户角色
+
+        }
 
     }
 }
