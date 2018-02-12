0d8c889bbb1bdc57d68573bfdc2baf1ea3bfeba8
Jing Zhang <v-jzingh@microsoft.com>
2018-02-12 15:40:34 +0800

    update2018

--- Praise2017/Praise2017/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Praise2017.MvcApplication" Language="C#" %>
---
+++ Praise2018/Praise2017/Controllers/AccountController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Praise2018.Models;
+using Praise2018.Abstract;
+
+
+namespace Praise2018.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Admin
+        private IAccountsRepo repo;
+        public AccountController (IAccountsRepo accountRepo)
+        {
+            this.repo = accountRepo;
+        }
+        public PartialViewResult UserList()
+        {
+            
+                //取出Name和头像
+                //var member = from b in db.Accounts
+
+                             //select b;
+
+           // IEnumerable people = member.ToArray();
+           // IEnumerable people = new IEnumerable(member);
+                return PartialView(repo);
+            
+        }
+
+        public PartialViewResult UserControlBtn()
+        {
+           
+            return PartialView();
+        }
+
+        public PartialViewResult AddUser()
+        {
+            return PartialView(new Account());
+        }
+
+          [HttpPost]
+        public PartialViewResult AddUser()
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    repo.Accounts.(accounts);
+                    db.SaveChanges();
+                }
+            }
+            catch (RetryLimitExceededException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            //return PartialView();
+
+            DeleteUser();
+        }
+
+        // 1. 分页
+        public PartialViewResult DeleteUser()
+        {
+            var member = from b in db.Accounts
+
+                         select b;
+            return PartialView(member);
+        }
+
+        //[HttpPost]
+        //public PartialViewResult DeleteUser()
+        //{
+        //    return PartialView();
+        //}
+    }
+}
